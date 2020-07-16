3657e827b250780d0004e61531fc910b20bc760e
yanolsh92 <yanolsh@gmail.com>
2020-07-16 18:55:44 +0300

    upodate menus to navigate withRouter

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './HomePage.scss';
+import Directory from '../../components/Directory/Directory';
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
+
+export default HomePage;
