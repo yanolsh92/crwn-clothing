728eb817234298f55ad76138e8422e2101a8f822
yanolsh92 <yanolsh@gmail.com>
2020-07-18 19:57:31 +0300

    stripe integration

--- src/pages/CheckoutPage/CheckoutPage.js
+++ src/pages/CheckoutPage/CheckoutPage.js
@@ -7,6 +7,7 @@ import {
   selectCartItems,
   selectCartTotal,
 } from '../../redux/cart/cart.selectors';
+import StripeCheckoutButton from '../../components/StripeButton/StripeButton';
 
 import './CheckoutPage.scss';
 const CheckoutPage = ({ cartItems, total }) => {
@@ -33,6 +34,12 @@ const CheckoutPage = ({ cartItems, total }) => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <div className='total'>TOTAL: ${total} </div>
+      <div className='test-warning'>
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/24 - CVV: 123
+      </div>
+      <StripeCheckoutButton price={total} />
     </div>
   );
 };
